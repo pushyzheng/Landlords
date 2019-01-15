630878aa9cc886e9488bcb7da66cc7742ab7c707
fx <991773829@qq.com>
2019-01-15 08:40:07 +0800

    notify

--- src/main/java/site/pushy/landlords/common/interceptor/WebSocketInterceptor.java
+++ src/main/java/site/pushy/landlords/common/interceptor/WebSocketInterceptor.java
@@ -1,11 +1,16 @@
 package site.pushy.landlords.common.interceptor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.HandshakeInterceptor;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
+
 
 /**
  * @author Fuxing
@@ -13,9 +18,20 @@ import java.util.Map;
  */
 public class WebSocketInterceptor implements HandshakeInterceptor {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
     @Override
-    public boolean beforeHandshake(ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse, WebSocketHandler webSocketHandler, Map<String, Object> map) throws Exception {
-        return false;
+    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler webSocketHandler, Map<String, Object> attributes) throws Exception {
+        // 将ServerHttpRequest转换成request请求相关的类，用来获取request域中的用户信息
+        if (request instanceof ServletServerHttpRequest) {
+            ServletServerHttpRequest servletRequest = (ServletServerHttpRequest) request;
+            HttpServletRequest httpRequest = servletRequest.getServletRequest();
+            String token = httpRequest.getParameter("token");
+            /* 解密token，拿到用户的userId */
+            String userId = "1faf0c33-0ead-415c-9bf1-4570e550e614";
+            attributes.put("userId", token);
+            logger.info(token + "连接到我了");
+        }
+        return true;
     }
 
     @Override
