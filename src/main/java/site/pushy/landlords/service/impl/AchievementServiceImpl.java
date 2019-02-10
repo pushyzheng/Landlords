aaba170a0fa94bc5d30171b3ca9285ed5aca311a
fx <991773829@qq.com>
2019-02-10 22:07:16 +0800

    achievement

--- src/main/java/site/pushy/landlords/service/impl/AchievementServiceImpl.java
+++ src/main/java/site/pushy/landlords/service/impl/AchievementServiceImpl.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import site.pushy.landlords.dao.AchievementMapper;
 import site.pushy.landlords.pojo.DO.Achievement;
+import site.pushy.landlords.pojo.DO.AchievementExample;
 import site.pushy.landlords.service.AchievementService;
+
+import java.util.List;
 
 /**
  * @author Pushy
@@ -18,11 +21,20 @@ public class AchievementServiceImpl implements AchievementService {
 
     @Override
     public Achievement getAchievementByUserId(String userId) {
-        return null;
+        AchievementExample achievementExample = new AchievementExample();
+        AchievementExample.Criteria criteria = achievementExample.createCriteria();
+        criteria.andUserIdEqualTo(userId);
+        List<Achievement> achievements = achievementMapper.selectByExample(achievementExample);
+        return achievements.isEmpty() ? null : achievements.get(0);
     }
 
     @Override
     public void updateAchievement(String userId, boolean isWinning) {
-
+        Achievement achievement = this.getAchievementByUserId(userId);
+        if (isWinning==true){
+            achievement.incrWinMatch();
+        }else {
+            achievement.decrFailureMatch();
+        }
     }
 }
